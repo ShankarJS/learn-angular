358d59eb460e4cffdb60d26badac65096a1b98e4
Shankarlal Sharma <shankarjsharma@gmail.com>
2023-10-21 19:44:30 +0530

    Added role dropdown
    Added role field assuming the register form to be some settings field.
    Also subscribed to valuechanges of role.

--- angular/src/app/monsterreactiveform/monsterreactiveform.component.ts
+++ angular/src/app/monsterreactiveform/monsterreactiveform.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -6,16 +6,27 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: './monsterreactiveform.component.html',
   styleUrls: ['./monsterreactiveform.component.css']
 })
-export class MonsterreactiveformComponent {
+export class MonsterreactiveformComponent implements OnInit {
 
   registerForm = this.fb.group({
     username: ['', Validators.required],
     password: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
+    roleId: [1, Validators.required]
   })
   isSubmitted = false;
+  roles = [
+    {id: 1, title: 'Developer'},
+    {id: 2, title: 'qa'}
+  ];
   
   constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.registerForm.get('roleId')?.valueChanges.subscribe( (roleId) => {
+      console.log('Send API request and update data', roleId);
+    });
+  }
 
   onSubmit(){
     console.log("Form submitted, data is: ", this.registerForm.value, this.registerForm.invalid);
